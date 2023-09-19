bb1cb067b8df9931dbd743cbf04ffa56a29cb809
ryosuke <kolor0211@gmail.com>
2023-09-19 22:37:48 +0900

    first commit

--- ryokadai04/ContentView.swift
+++ ryokadai04/ContentView.swift
@@ -8,14 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var resultNumber = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("\(resultNumber)")
+                .padding()
+            Button("+1") {
+                // カウントアップ
+                countUp()
+            }
+            .padding()
+            Button("clear") {
+                // クリア
+                countClear()
+            }
         }
         .padding()
+    }
+    
+    func countUp() {
+        resultNumber += 1
+    }
+    
+    func countClear() {
+        resultNumber = 0
     }
 }
 
