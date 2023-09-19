2c3440e00bbca1d68e5152f0da18e79943ec27d7
ryosuke <kolor0211@gmail.com>
2023-09-19 22:19:43 +0900

    Initial Commit

---
+++ ryokadai04/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ryokadai04
+//
+//  Created by 大田涼介 on 2023/09/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ryokadai04/ryokadai04App.swift
@@ -0,0 +1,17 @@
+//
+//  ryokadai04App.swift
+//  ryokadai04
+//
+//  Created by 大田涼介 on 2023/09/19.
+//
+
+import SwiftUI
+
+@main
+struct ryokadai04App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
